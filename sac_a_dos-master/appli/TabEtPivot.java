55153ead757067c3fc516ffb52cf02edff5fee7b
Skwyky <56221875+Skwyky@users.noreply.github.com>
2020-10-16 15:26:05 +0200

    Update TabEtPivot.java

--- sac_a_dos-master/appli/TabEtPivot.java
+++ sac_a_dos-master/appli/TabEtPivot.java
@@ -1,24 +1,27 @@
 package appli;
 
-//structure pour stocker la valeur de retour de la fonction de répartition du quicksort
-public class TabEtPivot { 
+//structure pour stocker la valeur de retour de la fonction de rÃ©partition du quicksort
+public class TabEtPivot {
+	
 	private Objet[] tab;
 	private int pivot;
-	
-	public Objet[] getTab(){
-		return this.tab;
-	}
-	
-	public int getPivot(){
-		return this.pivot;
-	}
 	
 	public void setTab(Objet[] tab){
 		this.tab = tab;
 	}
 	
+	public Objet[] getTab(){
+		return this.tab;
+	}
+	
+	
 	public void setPivot(int p){
 		this.pivot = p;
 	}
 
+	public int getPivot(){
+		return this.pivot;
+	}
+	
+	
 }
